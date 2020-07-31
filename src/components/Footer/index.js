9a27c4f14d7e33a0c8a8dbf697513199297ddc5b
Márcia Alves <marciasoalves@gmail.com>
2020-07-31 10:56:47 -0300

    Setup do projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png'
+
+function Footer() {
+  return (
+    <FooterBase>
+      <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
